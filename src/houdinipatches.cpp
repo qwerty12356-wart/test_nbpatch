c51db628a6d4632b2df88c12fab4edd5ff70d801
qwerty12356-wart <75067352+qwerty12356-wart@users.noreply.github.com>
2024-06-21 15:12:58 +0700

    Add performace patch to houdini

--- src/houdinipatches.cpp
+++ src/houdinipatches.cpp
@@ -29,3 +29,17 @@ void Patch_Permissive_Mprotect_Houdini13_39190(){
         debug_print("Patch_Permissive_Mprotect failed.");
     }
 }
+
+void Patch_Performance_Mprotect_Houdini13_39190(){
+    int res = 0;
+    #ifdef IS32BIT
+        res = PatchHex_32(nbbase, 0x282b64, 0xe8a784ec, 0x90909090);
+        res = res | PatchHex_8(nbbase, 0x282b68, 0xff, 0x90);
+    #else
+        res = PatchHex_32(nbbase, 0x2f3903, 0xe8588fed, 0x90909090);
+        res = res | PatchHex_8(nbbase,0x2f3907 , 0xff, 0x90);
+    #endif
+    if (res){
+        debug_print("Patch_Performace_Mprotect failed.");
+    }
+}
