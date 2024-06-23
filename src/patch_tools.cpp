51ee178da7ab2e1fae7ea8bee0a1e41e026e28fa
qwerty12356-wart <75067352+qwerty12356-wart@users.noreply.github.com>
2024-06-23 13:43:01 +0700

    Add more logging

--- src/patch_tools.cpp
+++ src/patch_tools.cpp
@@ -1,5 +1,6 @@
 #include "patch_tools.h"
 #include <cstdint>
+#include <android/log.h>
 #if INTPTR_MAX == INT32_MAX
     #define IS_32
 #else
@@ -8,7 +9,7 @@
 
 
 //Please don't jinx this up, compiler
-int PatchHex_32(void* baseaddress, long offset, uint32_t original_hex, uint32_t new_hex){
+int PatchHex_32(void* baseaddress, uint64_t offset, uint32_t original_hex, uint32_t new_hex){
 
 
     #ifdef IS_32
@@ -18,11 +19,13 @@ int PatchHex_32(void* baseaddress, long offset, uint32_t original_hex, uint32_t 
         return 0;
     }
     #else
-        uint32_t* addrtocheck = (uint32_t*)(char*)baseaddress + (uint64_t)offset; //Lol
+        uint64_t workaroundvar = (uint64_t)baseaddress + offset;
+        uint32_t* addrtocheck = (uint32_t*)workaroundvar; //Lol
         if (*addrtocheck == original_hex){
             *addrtocheck = new_hex;
             return 0;
         }
+        __android_log_print(ANDROID_LOG_DEBUG, "libnbpatcher", "Failed to patch hex %u at %ld", *addrtocheck, workaroundvar);
 
     #endif
     return 1;
