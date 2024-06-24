e03874111c43d696eca566a10848692ac1a5c333
qwerty12356-wart <75067352+qwerty12356-wart@users.noreply.github.com>
2024-06-24 20:40:14 +0700

    Trying random stuff

--- src/patch_tools.cpp
+++ src/patch_tools.cpp
@@ -3,6 +3,7 @@
 #include <cerrno>
 #include <android/log.h>
 #include "main.h"
+#include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
 #if INTPTR_MAX == INT32_MAX
@@ -23,6 +24,18 @@ int is_writeable(void *p)
     close(fd);
 
     return writeable;
+}
+
+int is_readable(void* p){
+    int dummypipe[2];
+    pipe(dummypipe);
+    int writeable = write(dummypipe[1], p, 1);
+    if (writeable == -1){
+        return 0;
+    }
+    close(dummypipe[0]);
+    close(dummypipe[1]);
+    return 1;
 }
 
 //Please don't jinx this up, compiler
@@ -68,7 +81,7 @@ int PatchHex_32(void* baseaddress, uint64_t offset, uint32_t original_hex, uint3
             return 1;
         }
         
-        if (!is_writeable(addrtocheck)){
+        if (!is_readable(addrtocheck)){
             error_print("Address is not writeable!");
             return 1;
         }
@@ -105,7 +118,7 @@ int PatchHex_8(void* baseaddress, int offset, uint8_t original_hex, uint8_t new_
             error_print("Patching out of range!");
             return 1;
         }
-        if (!is_writeable(addrtocheck)){
+        if (!is_readable(addrtocheck)){
             error_print("Address is not writeable!");
             return 1;
         }
